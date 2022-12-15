3024667ff2f94d444d848de9e818253c657b2934
Soroush mohammadi <sorooshm78@gmail.com>
2022-12-15 16:32:57 +0330

    initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # near_shops
+
+# Make a Location-Based Web App With Django and GeoDjango
+
+Check out [Make a Location-Based Web App With Django and GeoDjango](https://realpython.com/location-based-app-with-geodjango-tutorial/) for more information. 
---
+++ shops/models.py
@@ -0,0 +1,11 @@
+from django.contrib.gis.db import models
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.PointField()
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
